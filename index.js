77c305729a7ab6e9b7e830f5e64c4a88537b1740
rahem sorour <sorourrahem17@gmail.com>
2023-07-25 06:07:26 +0100

    completed assignement

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+function writeCards(name,event){
+    let ary=[]
+    for (let i=0;i<name.length;i++){
+        ary.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+  return ary
+}
+
+let listnames=["Guadalupe", "Ollie", "Aki"]
+console.log(writeCards(listnames, 'surprise'))
+
+function countDown(intg){
+    while(intg>=0){
+        console.log(intg--)
+       
+    }
+}
+
+countDown(10)
